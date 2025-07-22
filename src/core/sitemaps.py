9b38707a6e1022c10b40ac3a115e6e11a5945be0
jardahrazdera <jarek@jaroslav.tech>
2025-07-22 18:51:45 +0200

    feat: revert to simple sitemap.xml

--- src/core/sitemaps.py
+++ src/core/sitemaps.py
@@ -1,11 +1,12 @@
 # src/core/sitemaps.py
+
 from django.contrib.sitemaps import Sitemap
 from django.urls import reverse
 
 class StaticViewSitemap(Sitemap):
     protocol = 'https'
-    changefreq = "weekly"
     priority = 0.9
+    changefreq = 'weekly'
     i18n = True
 
     def items(self):
