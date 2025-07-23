40712746590abf1e2a676a3e53d3cb7f0267b471
jardahrazdera <jarek@jaroslav.tech>
2025-07-23 08:38:10 +0200

    hint: files cleaning and small improvements

--- src/core/admin.py
+++ src/core/admin.py
@@ -1,12 +1,6 @@
 # File: src/core/admin.py
 from django.contrib import admin
-
-# Register your models here.
-
-from django.contrib import admin
 from .models import SiteSettings
-
-# Register your models here.
 
 @admin.register(SiteSettings)
 class SiteSettingsAdmin(admin.ModelAdmin):
@@ -18,9 +12,7 @@ class SiteSettingsAdmin(admin.ModelAdmin):
     list_display = ('__str__', 'coming_soon_mode')
 
     def has_add_permission(self, request):
-        # Prevent adding new settings if one already exists.
         return not SiteSettings.objects.exists()
 
     def has_delete_permission(self, request, obj=None):
-        # Prevent deleting the settings object.
         return False
