112808a40feaa64f1637b7ebd9280f674efab83b
jardahrazdera <jarek@jaroslav.tech>
2025-09-20 09:38:48 +0200

    Create basic Django views for blog app
    - Add BlogListView for displaying published posts with ordering
    - Add BlogDetailView for individual post details
    - Filter both views to show only published posts (is_published=True)
    - Set proper template names and context object names
    - Include function-based view aliases for URL patterns

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
 
-# Create your views here.
+
+class BlogListView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+    queryset = Post.objects.filter(is_published=True)
+    ordering = ['-created_at']
+
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+    context_object_name = 'post'
+    queryset = Post.objects.filter(is_published=True)
+
+
+# Function-based view aliases for URL patterns
+post_list = BlogListView.as_view()
+post_detail = BlogDetailView.as_view()
